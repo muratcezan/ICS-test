ac1625e0f27f68718469bcbd0437b6af3c78ccdf
Murat CEZAN <muratcezan@gmail.com>
2020-10-20 01:58:13 +0300

    Added MainWindow obejct

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "hdr/mainwindow.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    MainWindow *Wind = new MainWindow(&engine);
 
     return app.exec();
 }
